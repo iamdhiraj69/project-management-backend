e156d639f53498def5da57a6322d630e1af0c30f
Dhiraj <cforcode69@gmail.com>
2025-09-20 11:39:14 +0530

    feat: add password comparison method to user schema

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -65,4 +65,8 @@ userSchema.pre("save", async function (next) {
   this.password = await bcrypt.hash(this.password, 10);
   next();
 });
+
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
 export const user = mongoose.model("User", userSchema);
