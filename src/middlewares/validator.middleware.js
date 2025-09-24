c81499c448dfcf33b6198cb0228285a540221d52
Dhiraj <cforcode69@gmail.com>
2025-09-24 21:32:07 +0530

    style: format code for consistency in validation middleware

--- src/middlewares/validator.middleware.js
+++ src/middlewares/validator.middleware.js
@@ -1,12 +1,16 @@
 import { validationResult } from "express-validator";
 import { ApiError } from "../utils/api-error.js";
 
+
+
 export const validate = (req, res, next) => {
-  const errors = validationResult(req);
-  if (errors.isEmpty()) return next();
-
-  const extractedErrors = [];
-  errors.array().map((err) => extractedErrors.push({ [err.path]: err.msg }));
-
-  return next(new ApiError(422, "Validation Error", extractedErrors));
+    const errors = validationResult(req);
+    if (errors.isEmpty()) {
+       return next()
+    }
+   const extractedErrors = []
+    errors.array().map((err) => extractedErrors.push({ [err.path]: err.msg }))
+    return next(new ApiError(422, "Validation Error", extractedErrors));
 };
+
+
