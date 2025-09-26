9440182f1f07ce795ff125a89b17a0d2c196a0c1
Dhiraj <cforcode69@gmail.com>
2025-09-26 20:19:28 +0530

    feat: add login functionality and cookie handling to auth controller and routes

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -57,8 +57,44 @@ const registerUser = asyncHandler(async (req, res) => {
   return res
     .status(201)
     .json(
-      new ApiResponse(201, {user: createdUser}, "User created successfully"),
+      new ApiResponse(201, { user: createdUser }, "User created successfully"),
     );
 });
 
-export { registerUser};
+const login = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    throw new ApiError(400, "Email and password are required");
+  }
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw new ApiError(401, "Invalid email or password");
+  }
+  const isPasswordCorrect = await user.isPasswordCorrect(password);
+  if (!isPasswordCorrect) {
+    throw new ApiError(401, "Invalid email or password");
+  }
+  const { accessToken, refreshToken } = await generateAccessAndRefreshTokens(
+    user._id,
+  );
+  const loggedInUser = await User.findById(user._id).select(
+    "-password -refreshToken -emailVerificationToken -emailVerificationExpiry",
+  );
+  const options = {
+    httpsOnly: true,
+    secure: true,
+  };
+  return res
+    .cookie("accessToken", accessToken, options)
+    .cookie("refreshToken", refreshToken, options)
+    .status(200)
+    .json(
+      new ApiResponse(
+        200,
+        { user: loggedInUser, accessToken, refreshToken },
+        "User logged in successfully",
+      ),
+    );
+});
+
+export { registerUser, login };
--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { registerUser } from "../controllers/auth.controller.js";
+import { registerUser, login } from "../controllers/auth.controller.js";
 import { validate } from "../middlewares/validator.middleware.js";
 import { userRegisterValidator } from "../validators/validator.js";
 
 const router = Router();
 
 router.route("/register").post(userRegisterValidator(), validate, registerUser);
+router.route("/login").post(login);
 
 export default router;
