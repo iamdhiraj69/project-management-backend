690338368bc58fe6c2a1d172184228a9c8cfa8cb
Dhiraj <cforcode69@gmail.com>
2025-10-08 20:44:42 +0530

    feat: add logout functionality to auth controller and routes

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -97,4 +97,26 @@ const login = asyncHandler(async (req, res) => {
     );
 });
 
-export { registerUser, login };
+const logoutUser = asyncHandler(async (req, res) => {
+  await User.findByIdAndUpdate(req.user._id, {
+    $set: {
+      refreshToken: null
+  
+    },
+  },
+  {
+      new: true,
+    
+    });
+  const options = {
+    httpsOnly: true,
+    secure: true
+  }
+  return res
+    .status(200)
+    .clearCookie("accessToken", options)
+    .clearCookie("refreshToken", options)
+    .json(new ApiResponse(200, null, "User logged out successfully"));
+});
+
+export { registerUser, login, logoutUser };
--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,11 +1,13 @@
 import { Router } from "express";
-import { registerUser, login } from "../controllers/auth.controller.js";
+import { registerUser, login, logoutUser } from "../controllers/auth.controller.js";
 import { validate } from "../middlewares/validator.middleware.js";
 import { userLoginValidator, userRegisterValidator } from "../validators/validator.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
 router.route("/register").post(userRegisterValidator(), validate, registerUser);
 router.route("/login").post(userLoginValidator(), validate, login);
+router.route("/logout").post(verifyJWT, logoutUser);
 
 export default router;
